a0c856dfeb0c6bfbcc2d8c7263642be46123507a
li1xiang <1614624236@qq.com>
2019-05-16 10:35:54 +0800

    20190516-V1.0-spring

---
+++ src/main/java/com/microo/erueka/EruekaApplication.java
@@ -0,0 +1,13 @@
+package com.microo.erueka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EruekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EruekaApplication.class, args);
+	}
+
+}
